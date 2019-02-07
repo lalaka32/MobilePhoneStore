aaa519991a126348fe09ecd2e170b097cd64cc85
lalaka32 <lalaka32@mail.ru>
2019-02-07 14:26:32 +0300

    Added PhoneViewModel

---
+++ MobileStore/Models/MobilePhoneModels/MobilePhoneViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileStore.Models.MobilePhoneModels
+{
+    public class MobilePhoneViewModel
+    {
+		public MobilePhone Phone{ get; set; }
+
+		public bool IsFavorite { get; set; }
+	}
+}
